b672fda274a8c00f191c3afa589d7ec00391722f
M Sai Preethi <122619685+saipreethi-msp@users.noreply.github.com>
2024-01-29 23:20:56 +0530

    Update data_explore_app.py

--- data_explore_app.py
+++ data_explore_app.py
@@ -1,33 +1,39 @@
 import streamlit as st 
+import pandas_profiling
 import plotly.express as px
 import pandas as pd 
 
+
+from streamlit_pandas_profiling import st_profile_report
+
 # load data 
 st.title("Exploration App")
-st.tilte("sai preethi")
+# menu
+options = st.sidebar.selectbox('Menu',("Profile Report","Prediction"))
+if options == "Profile Report":
+    file = st.file_uploader("Upload a dataset", type=['csv'])
+    st.subheader("Profile Report")
+    if file is not None:
 
-file = st.file_uploader("Upload a dataset", type=['csv'])
-if file is not None:
-
-    df = pd.read_csv(file)
-
-    # streamlit app structure
-
-    # Data Display
-    if st.checkbox("Show Dataset"):
+        df = pd.read_csv(file)
+        
+        # Data Display
         st.write(df)
 
-    # User inputs for plot customization
-    x_axis_val = st.selectbox('Select the X axis', options=df.columns)
-    y_axis_val = st.selectbox('Select the Y axis', options=df.columns)
-    plot_type = st.selectbox('Select plot type', ['scatter','line','bar','asdasd'])
+        report = df.profile_report()
+        st_profile_report(report)
+        # User inputs for plot customization
+        x_axis_val = st.selectbox('Select the X axis', options=df.columns)
+        y_axis_val = st.selectbox('Select the Y axis', options=df.columns)
+        plot_type = st.selectbox('Select plot type', ['scatter','line','bar',])
 
-    #creating a plotly chart
-    if plot_type == 'scatter':
-        fig = px.scatter(df, x= x_axis_val, y = y_axis_val)
-    elif plot_type == 'line':
-        fig = px.line(df, x= x_axis_val, y = y_axis_val)
-    elif plot_type == 'bar':
-        fig = px.bar(df, x= x_axis_val, y = y_axis_val)
-    st.plotly_chart(fig)
-    # to run the code    streamlit run abcd.py
+
+        #creating a plotly chart
+        if plot_type == 'scatter':
+            fig = px.scatter(df, x= x_axis_val, y = y_axis_val)
+        elif plot_type == 'line':
+            fig = px.line(df, x= x_axis_val, y = y_axis_val)
+        elif plot_type == 'bar':
+            fig = px.bar(df, x= x_axis_val, y = y_axis_val)
+        st.plotly_chart(fig)
+        # to run the code    streamlit run abcd.py
